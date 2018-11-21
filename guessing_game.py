d4542ce6563e31d00e3ca2e7c0d6919de999ee10
Logicae <17463839+Logicae@users.noreply.github.com>
2018-11-20 20:46:19 -0500

    cleared comments

--- guessing_game.py
+++ guessing_game.py
@@ -1,13 +1,6 @@
 import random
 
 random_number = random.randint(1,10)  # numbers 1 - 10
-
-# handle user guesses
-#   if they guess correctly, tell them they won
-#     otherwise tell them if they are too high or too low
-
-# BONUS - let player play again if they want!
-
 user_guess = ""
 
 while user_guess != random_number:  
